8ca908385c971245459fe7fd1057ccdeda1db9fa
Luca Cordes <cordes135@gmail.com>
2023-10-26 18:32:58 +0200

    10min update time restrict to 120 data points

--- public/script.js
+++ public/script.js
@@ -1,6 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const moistureList = document.getElementById('moisture-list');
     const chartContainer = document.getElementById('chart-container');
+    const chartWidth = window.innerWidth * 0.9; // Set width to 90% of the viewport width
+    const chartHeight = window.innerHeight * 0.5; // Set height to 50% of the viewport height
 
     function fetchData() {
         const currentTime = Date.now(); // Get the current timestamp in milliseconds
@@ -16,8 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     // Calculate the difference between current time and entry's timestamp
                     const timeDifference = currentTime - (typeof timestamp === 'number' ? timestamp : new Date(timestamp).getTime());
     
-                    // Include data only if it is not older than 10 days
-                    if (timeDifference <= 60000*60*10) {
+                    // Include data only if it is not older than 20 hours
+                    if (timeDifference <= 60000*60*20) {
                         const listItem = document.createElement('li');
                         listItem.textContent = `Plant: ${plantname}, Moisture: ${moisturePercentage}%, Time: ${new Date(timestamp).toLocaleString()}`;
                         moistureList.appendChild(listItem);
@@ -29,18 +31,18 @@ document.addEventListener('DOMContentLoaded', () => {
                         chartData[plantname].push({ x: timestampInMillis, y: moisturePercentage });
     
                         // Keep only the last 10 data points for each plant
-                        if (chartData[plantname].length > 100) {
+                        if (chartData[plantname].length > 120) {
                             chartData[plantname].shift();
                         }
                     }
                 });
-    
-                // Update Chart.js plots
-                updateCharts(chartData);
-            })
+
+        // Update Chart.js plots
+        updateCharts(chartData);
+})
             .catch(error => console.error('Error:', error));
     }
-    
+
 
     function updateCharts(data) {
         // Destroy existing charts if any
@@ -49,9 +51,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 chart.destroy();
             });
         }
-    
-        const chartContainer = document.getElementById('chart-container');
-    
+        chartContainer.innerHTML = '';
+           
+
         Object.keys(data).forEach(plantname => {
             const plantData = data[plantname].map(entry => {
                 return {
@@ -59,16 +61,13 @@ document.addEventListener('DOMContentLoaded', () => {
                     y: entry.y
                 };
             });
-    
-            // Set custom dimensions for the chart
-            const chartWidth = 800; // Set your desired width
-            const chartHeight = 100; // Set your desired height
-    
+
+            // Create a new canvas element for each chart
             const ctx = document.createElement('canvas');
             ctx.width = chartWidth;
             ctx.height = chartHeight;
             chartContainer.appendChild(ctx);
-    
+
             const myChart = new Chart(ctx, {
                 type: 'line',
                 data: {
@@ -91,15 +90,15 @@ document.addEventListener('DOMContentLoaded', () => {
                     }
                 }
             });
-    
+
             if (!window.myCharts) {
                 window.myCharts = [];
             }
-    
+
             window.myCharts.push(myChart);
         });
     }
-    
+
 
     function getRandomColor() {
         const letters = '0123456789ABCDEF';
@@ -110,8 +109,8 @@ document.addEventListener('DOMContentLoaded', () => {
         return color;
     }
 
-    // Fetch data every 60 seconds
-    setInterval(fetchData, 60000);
+    // Fetch data every 10 min
+    setInterval(fetchData, 60000*10);
 
     // Fetch data on page load
     fetchData();
